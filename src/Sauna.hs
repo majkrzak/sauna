493722dd1bcffd98e9629d3050b93da58c1fbd4d
Piotr Majkrzak <piotr@majkrzak.dev>
2022-02-06 19:18:22 +0100

    Add terribly unoptimal but briliant eliminationScore

--- src/Sauna.hs
+++ src/Sauna.hs
@@ -9,106 +9,13 @@ import Sauna.Data.Letter
 import Sauna.Data.Word
 import Data.Quintuple
 import Data.List (union, (\\), intersect, nub, sort, sortBy)
-import Data.Foldable (toList, maximumBy)
+import Data.Foldable (toList, maximumBy, minimumBy)
 import Data.FileEmbed (embedFile)
 import Data.ByteString.UTF8 (toString)
 import Control.Applicative (liftA3, liftA2)
 import Data.Wrapper
 import Sauna.Data.Dictionary
 import Data.Monoid (Sum(Sum), getSum)
-
-
-------------
----- Filters
-------------
---
---type WordFilter = Word -> Bool
---
----- | Filters Words contains all the Letters.
---presentFilter :: [Letter] -> WordFilter
---presentFilter alphabet word = coverage alphabet $ toList $ unwrap word
---  where
---    coverage :: [Letter] -> [Letter] -> Bool
---    coverage (l:ls) ws = (l `elem` ws) && coverage ls (ws\\[l])
---    coverage [] _ = True
---
----- | Filter Words matching options.
---optionsFilter :: Quintuple [Letter] -> WordFilter
---optionsFilter options word = all (uncurry elem) (liftA2 (,) (unwrap word) options)
---
----- | Filters possible solutions.
---solutionFilter :: State -> WordFilter
---solutionFilter State{..} word = presentFilter present  word && optionsFilter options word
---
----- | Filter words over given alphabet.
---alphabetFilter :: [Letter] -> WordFilter
---alphabetFilter alphabet word = word' == word' `intersect` alphabet
---  where
---    word' = toList $ unwrap word
---
----- | Filters words containing n-diferent letters.
---lengthFilter :: Int -> WordFilter
---lengthFilter n word = n == length (nub word')
---  where
---    word' = toList $ unwrap word
---
---
-----------
----- Utils
-----------
---
----- | Creates minimal alphabet covering given dictionary.
---dictionaryAlphabet :: Dictionary -> [Letter]
---dictionaryAlphabet (Dictionary (word:words)) = nub $ union word' (dictionaryAlphabet (wrap words))
---  where
---    word' = toList $ unwrap word
---dictionaryAlphabet (Dictionary []) = []
---
---------
----- API
---------
---
---init :: State
---init = State options' present' unused'
---  where
---    options' = pure [A ..]
---    present' = []
---    unused' = [A ..]
---
---next :: State -> Word
---next state@State{unused} = let
---    solutionDictionary = wrap $ filter (solutionFilter state) $ unwrap dictionary
---    alphabet = dictionaryAlphabet solutionDictionary
---    coverageDictionary = wrap $ filter (alphabetFilter (alphabet `intersect` unused)) $ unwrap dictionary
---    coverageDictionary5 = wrap $ filter (lengthFilter 5) $ unwrap coverageDictionary
---    coverageDictionary4 = wrap $ filter (lengthFilter 4) $ unwrap coverageDictionary
---    coverageDictionary3 = wrap $ filter (lengthFilter 3) $ unwrap coverageDictionary
---  in head $ unwrap coverageDictionary5 <> unwrap coverageDictionary4 <> unwrap coverageDictionary3 <> unwrap solutionDictionary
---
---update :: State -> Word -> Response -> State
---update
--- State {..}
--- word
--- response
--- = State options' present' unused'
--- where
---   options' :: Quintuple [Letter]
---   options' = fmap ( \case
---      (letter, Green, _) -> [letter]
---      (_, _, [letter]) -> [letter]
---      (letter, _, option) -> (option \\ ((blacks) \\ yellows)) \\ [letter]
---     ) (liftA3 (,,) (unwrap word) (unwrap response) options)
---   present' = present <> (present'' \\ present)
---   present'' = foldMap ( \case
---      (letter, Green) -> [letter]
---      (letter, Yellow) -> [letter]
---      (_,_) -> []
---    ) (liftA2 (,) (unwrap word) (unwrap response))
---   unused' = unused \\ toList (unwrap word)
---   letters color = foldMap ( \(letter, color') -> [letter | color == color'] ) (liftA2 (,) (unwrap word) (unwrap response))
---   greens = letters Green
---   blacks = letters Black
---   yellows = letters Yellow
 
 
 fullAlphabet :: Alphabet
@@ -172,6 +79,10 @@ solutionFilter state word' = presentFilter (present state)  word' && optionsFilt
     optionsFilter :: Quintuple Alphabet -> WordFilter
     optionsFilter opts word = all (uncurry elem) (liftA2 (,) (unwrap word) (unwrap <$> opts))
 
+-- | Dictionary of valid solutions for given State.
+solutionDictionary :: State -> Dictionary
+solutionDictionary state = wrap $ filter (solutionFilter state) $ unwrap fullDictionary
+
 -- | Type for ordering dictionaries.
 type WordOrdering = Word -> Word -> Ordering
 
@@ -189,6 +100,15 @@ overlapScore state word = foldMap score' (filter (solutionFilter state) (unwrap 
       where
         score'' :: Letter -> Sum Int
         score'' l = Sum $ length (filter (l==) (nub (toList (unwrap word))))
+
+
+-- | Orders Words by count of worst case solutions.
+eliminationOrdering :: State -> WordOrdering
+eliminationOrdering state a b =  compare (eliminationScore state a) (eliminationScore state b)
+
+-- | Counts solutions in worst case scenario.
+eliminationScore :: State -> Word -> Int
+eliminationScore state word = maximum [length (unwrap (solutionDictionary (update state word response))) | response <- [Response (Quintuple (x1,x2,x3,x4,x5)) | x1 <- [Black ..], x2 <- [Black ..],x3 <- [Black ..],x4 <- [Black ..],x5 <- [Black ..]]]
 
 init :: State
 init = wrap []
