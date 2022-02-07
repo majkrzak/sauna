8a4d33ce49336f7e2f55cf2f8039262bf8c2d56a
Piotr Majkrzak <piotr@majkrzak.dev>
2022-02-07 20:05:28 +0100

    Cleanup Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,18 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-
 module Main where
 
 import Sauna
 import Sauna.Data.Color
 import Sauna.Data.Response
-import Sauna.Data.State
-
 import Prelude hiding (Word, init, lookup)
-
-import System.IO (hFlush, stdout, stderr,hPutStr, hPrint)
-import Data.Wrapper (unwrap)
-import Control.Concurrent.ParallelIO
+import System.IO (hFlush,stdout)
 
 
 
 main :: IO ()
 main = do
-  --print [(word, getSum $ overlapScore init word) | word <- sortBy (overlapOrdering init) (unwrap fullDictionary)]
   loop initialize
   where
     loop state = do
-      stats state
       let word = guess state
       print word
       hFlush stdout
@@ -30,23 +21,3 @@ main = do
         loop $ next state (word,response)
       else
         loop initialize
-
-
---
-stats :: State -> IO ()
-stats state = do
---  hPrint stderr state
-    hPutStr stderr "solutionFilter: :"
-    hPrint stderr $ length $ filter (solutionFilter state) $ unwrap fullDictionary
-    hPrint stderr $ filter (solutionFilter state) $ unwrap fullDictionary
-    hPutStr stderr "options:        :"
-    hPrint stderr $ options state
-    hPutStr stderr "unused:         :"
-    hPrint stderr $ unused state
---  hPutStr stderr "presentFilter:  :"
---  hPrint stderr $ length $ filter (presentFilter $ present state) $ unwrap dictionary
---  hPrint stderr $ filter (presentFilter $ present state) $ unwrap dictionary
---  hPutStr stderr "optionsFilter:  :"
---  hPrint stderr $ length $ filter (optionsFilter $ options state) $ unwrap dictionary
---  hPrint stderr $ filter (optionsFilter $ options state) $ unwrap dictionary
-
